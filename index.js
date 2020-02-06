8ee0f920fd1fd0ec7bf3078d48a0a3a44d36e303
PawelGajo <pudzian12312@gmail.com>
2020-02-06 23:47:56 +0100

    req.params instead of req.body

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.listen(8000, () => console.log('Server started'));
 
 app.get('/page/:id', async (req, res) => {
-  const ret = await pageParser.getContentOfPages(req.body.id, pages);
+  const ret = await pageParser.getContentOfPages(req.params.id, pages);
   res.send(ret);
 })

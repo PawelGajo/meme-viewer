b6c2e3829e0d3c9ed29b9da36c2840ccde88d59e
PawelGajo <pudzian12312@gmail.com>
2020-02-06 23:40:49 +0100

    add express and get request

--- index.js
+++ index.js
@@ -1,48 +1,15 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const pages = require('./pagesConfig');
+const pp = require('./pageParser');
+const pageParser = new pp();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.listen(8000, () => console.log('Server started'));
 
-let jbzdPage = 1;
-let kwejkPage = null;
-async function fetchJbzdData() {
-  let driver = await new Builder().forBrowser('firefox').build(); 
-  await driver.manage().setTimeouts( { implicit: 10000, pageLoad: 
-    10000, script: 10000 } );
-  let a = null;
-  try {
-    await driver.get(`https://jbzd.com.pl/str/${jbzdPage}`);
-    a = await driver.findElements(By.css('div.article-image a img, div.article-image video source'));
-    for (test of a) {
-    console.log(await test.getAttribute("src"));
-    }  
-  } catch(e) {
-      console.error(e);
-  }                  
-  driver.close();     
-}
-async function fetchKwejkData() {
-  let driver = await new Builder().forBrowser('firefox').build(); 
-  await driver.manage().setTimeouts( { implicit: 10000, pageLoad: 
-    10000, script: 10000 } );
-  let a = null;
-  try {
-    if ( ! kwejkPage ) {
-      await driver.get(`https://kwejk.pl`);
-      kwejkPage = await driver.findElement(By.css(`.pager .current a`)).getText();
-      console.log(kwejkPage);
-    } else {
-      await driver.get(`https://kwejk.pl/strona/${kwejkPage}`);
-    }
-    a = await driver.findElements(By.css('div.media-element'));
-    for (test of a) {
-    console.log(await test.getAttribute("data-image"));
-    }
-    kwejkPage--; 
-  } catch(e) {
-      console.error(e);
-  }                  
-  driver.close();     
-}
-fetchKwejkData().then(() =>
-{
-  fetchKwejkData();
-});
+app.get('/page/:id', async (req, res) => {
+  const ret = await pageParser.getContentOfPages(1, pages);
+  res.send(ret);
+})

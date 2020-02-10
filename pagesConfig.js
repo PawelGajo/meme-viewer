1bf2e78c277f95381a4f565a5236a26220e1ac39
PawelGajo <pudzian12312@gmail.com>
2020-02-10 23:16:54 +0100

    change config structure

--- pagesConfig.js
+++ pagesConfig.js
@@ -1,33 +1,42 @@
 const pages ={
     JBZD: {
         name: "JBZD",
+        logo: "https://vignette.wikia.nocookie.net/logopedia/images/e/ef/Jbzd-logo1.jpg/revision/latest/scale-to-width-down/340?cb=20151205160123",
         uri: 'https://jbzd.com.pl',
         separator: '/str/',
-        content: {
-            query: 'div.article-image a img, div.article-image video source',
-            attribute: 'src',
+        query: {
+            container: 'div.article-content',
+            description: 'alt',
+            img: '.article-image a img', // TODO a img, video source 
+            imgAttribute: 'src',
+
         },
-        pageNumberQuery: '.paginator > table > tbody > tr > td > span > strong',
         imagesPrefix: '',
     },
     KWEJK: {
         name: "KWEJK",
+        logo: "https://kwejk.pl/images/logo.png",
         uri: 'https://kwejk.pl',
         separator: '/strona/',
-        content: {
-            query: 'div.media-element',
-            attribute: 'data-image',
+        query: {
+            container: 'div.media-element',
+            description: 'alt',
+            img: '.figure img',
+            imgAttribute: 'src',
         },
         pageNumberQuery: 'div.pagination ul li.current a',
         imagesPrefix: '',
     },
     MISTRZOWIE: {
         name: "MISTRZOWIE",
+        logo: "https://mistrzowie.org/res/services/mistrzowie/img/layout/logo_smallheader.gif",
         uri: 'https://mistrzowie.org',
         separator: '/page/',
-        content: {
-            query: 'img.pic',
-            attribute: 'src',
+        query: {
+            container: '.box.fav.picture',
+            description: 'alt',
+            img: 'img.pic',
+            imgAttribute: 'src',
         },
         imagesPrefix: 'https://mistrzowie.org',
     }

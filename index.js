7b2844425caa89ca1f3bf37df176a8f09b44cf4d
PawelGajo <pudzian12312@gmail.com>
2020-02-13 22:42:12 +0100

    remove unnecessary code.

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ const pages = require('./pagesConfig');
 const pp = require('./pageParser');
 const pageParser = new pp();
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 app.use(cors(corsOptions))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 app.listen(8000, () => console.log('Server started'));
 
 app.get('/page/:id', async (req, res) => {

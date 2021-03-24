e8fc9121655dfef4561992339a61c2e272ba5788
Gajo Pawel <Pawel.Gajo@snt.pl>
2021-03-24 23:30:23 +0100

    change app.listen line

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors(corsOptions))
-app.listen(8000, () => console.log('Server started'));
 
 app.get('/page/:id', async (req, res) => {
   const ret = await pageParser.getContentOfPages(req.params.id, pages);
   res.send(ret);
 })
+
+app.listen(8000, () => console.log('Server started'));
